767cfbc70386a689437c91894595d0eab710c4bc
Evan Guirino <etg624@gmail.com>
2018-12-06 11:11:20 -0800

    adde cat features

--- evenListenerDrills.js
+++ evenListenerDrills.js
@@ -1,5 +1,17 @@
 
-$('.thumbnails').on('click', function(){
-    $('.thumbnail').attr('alt', 'we change the alt')
-    console.log('image click');
-})
+function handleThumbnailClicks() {
+    $('.thumbnail').on('click', function(event) {
+       const imgSrc = $(this).find('img').attr('src');  
+       const imgAlt = $(this).find('img').attr('alt');
+      
+      $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+      
+      // Accomplishes the same as line 6 with only one use off .attr()
+      // by passing it an object with multiple properties.
+      // See: http://api.jquery.com/attr/#attr-attributes
+      // $('.hero img').attr({'src': imgSrc, 'alt': imgAlt});
+    });
+  }
+
+
+  handleThumbnailClicks();
